8af42bca91479d85714899ef553bb9548d5556b9
lara <lahirurajapakshe.stack@gmail.com>
2022-11-20 14:20:15 +0530

    Inittial commit

---
+++ src/main/java/online/lahiru/sprinngbotrestapi/SprinngbotRestApiApplication.java
@@ -0,0 +1,13 @@
+package online.lahiru.sprinngbotrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinngbotRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinngbotRestApiApplication.class, args);
+	}
+
+}
