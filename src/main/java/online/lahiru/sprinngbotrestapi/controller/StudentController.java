fa893554787e21a7657d617cd686012fa055ffd9
lara <lahirurajapakshe.stack@gmail.com>
2022-11-20 16:43:46 +0530

    Test path variable

--- src/main/java/online/lahiru/sprinngbotrestapi/controller/StudentController.java
+++ src/main/java/online/lahiru/sprinngbotrestapi/controller/StudentController.java
@@ -2,6 +2,7 @@ package online.lahiru.sprinngbotrestapi.controller;
 
 import online.lahiru.sprinngbotrestapi.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -10,20 +11,24 @@ import java.util.List;
 @RestController
 public class StudentController {
     @GetMapping("student")
-    public Student getStudent(){
+    public Student getStudent() {
         Student student = new Student(1, "lahiru", "San");
         return student;
 
     }
 
     @GetMapping("listStu")
-    public List<Student> getStudents(){
-        List<Student> students= new ArrayList<>();
-        students.add(new Student(1,"lara","rj"));
-        students.add(new Student(2,"jaya","sakis"));
-        students.add(new Student(3,"kakka","bara"));
+    public List<Student> getStudents() {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1, "lara", "rj"));
+        students.add(new Student(2, "jaya", "sakis"));
+        students.add(new Student(3, "kakka", "bara"));
 
-        return  students;
+        return students;
     }
+@GetMapping("listStu/{id}")
+    public Student studentPathVariable(@PathVariable("id") int stuId) {
+        return new Student(stuId, "LARA", "RJ");
 
+    }
 }
