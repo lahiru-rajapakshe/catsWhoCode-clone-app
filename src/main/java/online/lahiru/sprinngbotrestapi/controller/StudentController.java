8770abd6b5cfe48f7434585f677c3891978a30c5
lara <lahirurajapakshe.stack@gmail.com>
2022-11-20 17:46:57 +0530

    Test @PostMapping & @ResponceStatus

--- src/main/java/online/lahiru/sprinngbotrestapi/controller/StudentController.java
+++ src/main/java/online/lahiru/sprinngbotrestapi/controller/StudentController.java
@@ -1,10 +1,8 @@
 package online.lahiru.sprinngbotrestapi.controller;
 
 import online.lahiru.sprinngbotrestapi.bean.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,5 +35,14 @@ public class StudentController {
     @GetMapping("listStu/query")
     public Student studentRequestVariable(@RequestParam int id){
         return  new Student(id,"Ramesh","fadatare");
+    }
+
+    @PostMapping("listStu/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student createStudent(@RequestBody Student student){
+        System.out.println(student.getId());
+        System.out.println(student.getFullName());
+        System.out.println(student.getLastName());
+        return  student;
     }
 }
