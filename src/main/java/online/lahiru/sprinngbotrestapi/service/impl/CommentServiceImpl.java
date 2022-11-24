5a5631d815767e02606054f5dcd125368b11efd1
lara <lahirurajapakshe.stack@gmail.com>
2022-11-24 15:54:17 +0530

    Implement createComment on CommentServiceImpl

--- src/main/java/online/lahiru/sprinngbotrestapi/service/impl/CommentServiceImpl.java
+++ src/main/java/online/lahiru/sprinngbotrestapi/service/impl/CommentServiceImpl.java
@@ -1,23 +1,39 @@
 package online.lahiru.sprinngbotrestapi.service.impl;
 
 import online.lahiru.sprinngbotrestapi.entity.Comment;
+import online.lahiru.sprinngbotrestapi.entity.Post;
+import online.lahiru.sprinngbotrestapi.exception.ResourceNotFoundException;
 import online.lahiru.sprinngbotrestapi.payload.CommentDTO;
 import online.lahiru.sprinngbotrestapi.repository.CommentRepository;
+import online.lahiru.sprinngbotrestapi.repository.PostRepository;
 import online.lahiru.sprinngbotrestapi.service.CommentService;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CommentServiceImpl implements CommentService {
 
     private CommentRepository commentRepository;
+    private PostRepository postRepository;
 
-    public CommentServiceImpl(CommentRepository commentRepository) {
+    public CommentServiceImpl(CommentRepository commentRepository,PostRepository postRepository) {
         this.commentRepository = commentRepository;
+        this.postRepository=postRepository;
     }
 
     @Override
     public CommentDTO createComment(long postId, CommentDTO commentDTO) {
-        return null;
+        Comment comment = mapToEntity(commentDTO);
+
+        Post post = postRepository.findById(postId).orElseThrow(()->new ResourceNotFoundException("Post","id",postId));
+        comment.setPost(post);
+        Comment newComment = commentRepository.save(comment);
+
+        
+
+
+        return mapToDTO(newComment);
     }
 
     private CommentDTO mapToDTO(Comment comment){
