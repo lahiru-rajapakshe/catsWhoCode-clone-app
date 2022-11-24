8f9a1424ab40d11b0e803eaeed6a0ab9d10174ae
lara <lahirurajapakshe.stack@gmail.com>
2022-11-24 14:19:34 +0530

    Refactor postController by creating a Application level class

--- src/main/java/online/lahiru/sprinngbotrestapi/controller/PostController.java
+++ src/main/java/online/lahiru/sprinngbotrestapi/controller/PostController.java
@@ -3,6 +3,7 @@ package online.lahiru.sprinngbotrestapi.controller;
 import online.lahiru.sprinngbotrestapi.payload.PostDTO;
 import online.lahiru.sprinngbotrestapi.payload.PostResponse;
 import online.lahiru.sprinngbotrestapi.service.PostService;
+import online.lahiru.sprinngbotrestapi.utils.AppConstants;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -28,10 +29,10 @@ public class PostController {
     //get all post rest API
     @GetMapping
     public PostResponse getAllPosts(
-            @RequestParam(value = "pageNo", defaultValue = "0",required = false) int pageNo,
-            @RequestParam(value = "pageSize",defaultValue = "10",required = false) int pageSize,
-            @RequestParam(value = "sortBY",defaultValue = "id",required = false) String sortBy,
-            @RequestParam(value = "sortDir",defaultValue = "asc",required = false)String sortDir
+            @RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER,required = false) int pageNo,
+            @RequestParam(value = "pageSize",defaultValue = AppConstants.DEFAULT_PAGE_SIZE,required = false) int pageSize,
+            @RequestParam(value = "sortBY",defaultValue = AppConstants.DEFAULT_SORT_BY,required = false) String sortBy,
+            @RequestParam(value = "sortDir",defaultValue = AppConstants.DEFAULT_SORT_DIRECTION,required = false)String sortDir
     ) {
         return postService.getAllPosts(pageNo,pageSize,sortBy,sortDir);
     }
