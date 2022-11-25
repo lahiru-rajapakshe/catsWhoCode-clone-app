9815465024bc043c74b2a29706ec11e812ae4188
lara <lahirurajapakshe.stack@gmail.com>
2022-11-25 06:45:59 +0530

    Implement getCommentById on CommentServiceImpl

--- src/main/java/online/lahiru/sprinngbotrestapi/service/impl/CommentServiceImpl.java
+++ src/main/java/online/lahiru/sprinngbotrestapi/service/impl/CommentServiceImpl.java
@@ -2,13 +2,16 @@ package online.lahiru.sprinngbotrestapi.service.impl;
 
 import online.lahiru.sprinngbotrestapi.entity.Comment;
 import online.lahiru.sprinngbotrestapi.entity.Post;
+import online.lahiru.sprinngbotrestapi.exception.BlogAPIException;
 import online.lahiru.sprinngbotrestapi.exception.ResourceNotFoundException;
 import online.lahiru.sprinngbotrestapi.payload.CommentDTO;
 import online.lahiru.sprinngbotrestapi.repository.CommentRepository;
 import online.lahiru.sprinngbotrestapi.repository.PostRepository;
 import online.lahiru.sprinngbotrestapi.service.CommentService;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.BadLocationException;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -41,6 +44,17 @@ public class CommentServiceImpl implements CommentService {
         List<Comment> comments = commentRepository.findByPostId(postId);
         return comments.stream().map(comment -> mapToDTO(comment)).collect(Collectors.toList());
 
+    }
+
+    @Override
+    public CommentDTO getCommentById(Long postId, Long commentId) {
+        Post post = postRepository.findById(postId).orElseThrow(() -> new ResourceNotFoundException("Post", "id", postId));
+        Comment comment=commentRepository.findById(commentId).orElseThrow(()->new ResourceNotFoundException("Comment","id", commentId));
+
+        if(!comment.getPost().getId().equals(post.getId())){
+            throw  new BlogAPIException(HttpStatus.BAD_REQUEST,"Comment does not belongs to the post");
+        }
+        return mapToDTO(comment);
     }
 
     private CommentDTO mapToDTO(Comment comment) {
