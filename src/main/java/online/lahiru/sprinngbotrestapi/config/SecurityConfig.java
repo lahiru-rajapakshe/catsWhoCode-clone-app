74001bc3f4fe246161269c27cadc2c83fa29a795
lara <lahirurajapakshe.stack@gmail.com>
2022-11-25 22:13:10 +0530

    Implement mock users for SecurityConfig

--- src/main/java/online/lahiru/sprinngbotrestapi/config/SecurityConfig.java
+++ src/main/java/online/lahiru/sprinngbotrestapi/config/SecurityConfig.java
@@ -1,22 +1,48 @@
 package online.lahiru.sprinngbotrestapi.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .csrf().disable()
+//                .authorizeRequests()
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .httpBasic();
+
         http
                 .csrf().disable()
                 .authorizeRequests()
+                .antMatchers(HttpMethod.GET,"/api/**").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    protected UserDetailsService userDetailsService() {
+        UserDetails lahiru = User.builder().username("lahiru").password("1234").roles("USER").build();
+        UserDetails admin = User.builder().username("admin").password("admin").roles("ADMIN").build();
+
+        return  new InMemoryUserDetailsManager(lahiru,admin);
+
+        
     }
 }
