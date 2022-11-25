d9102f2b37535b7865e18531e5cc3bfeb82eb6da
lara <lahirurajapakshe.stack@gmail.com>
2022-11-25 05:33:52 +0530

    Add getCommentsByPostId on CommentService

--- src/main/java/online/lahiru/sprinngbotrestapi/service/CommentService.java
+++ src/main/java/online/lahiru/sprinngbotrestapi/service/CommentService.java
@@ -2,6 +2,10 @@ package online.lahiru.sprinngbotrestapi.service;
 
 import online.lahiru.sprinngbotrestapi.payload.CommentDTO;
 
+import java.util.List;
+
 public interface CommentService {
     CommentDTO createComment(long postId,CommentDTO commentDTO);
+
+    List<CommentDTO> getCommentsByPostId(long postId);
 }
