c79cd25069d1e6832d1349ffec3f42f0f712fc04
lara <lahirurajapakshe.stack@gmail.com>
2022-11-25 10:35:16 +0530

    Add model-mapper configuration

--- src/main/java/online/lahiru/sprinngbotrestapi/SprinngbotRestApiApplication.java
+++ src/main/java/online/lahiru/sprinngbotrestapi/SprinngbotRestApiApplication.java
@@ -1,11 +1,17 @@
 package online.lahiru.sprinngbotrestapi;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SprinngbotRestApiApplication {
 
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SprinngbotRestApiApplication.class, args);
 	}
