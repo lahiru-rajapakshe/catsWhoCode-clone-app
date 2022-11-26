edad389353b2db10ac7e5acc94b714fe42fa61c1
lara <lahirurajapakshe.stack@gmail.com>
2022-11-27 00:03:19 +0530

    Implement Docket method

--- src/main/java/online/lahiru/sprinngbotrestapi/config/SwaggerConfig.java
+++ src/main/java/online/lahiru/sprinngbotrestapi/config/SwaggerConfig.java
@@ -1,8 +1,13 @@
 package online.lahiru.sprinngbotrestapi.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 import java.util.Collections;
 
@@ -23,5 +28,13 @@ public class SwaggerConfig {
                 Collections.emptyList()
         );
     }
-
+    @Bean
+public Docket api(){
+        return  new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+}
 }
